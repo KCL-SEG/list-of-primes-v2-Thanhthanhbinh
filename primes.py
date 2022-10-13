ccbdca64d46c752d807deb65f762177a7f91ea3b
Binh Do <binh.do@kcl.ac.uk>
2022-10-13 11:59:09 +0100

    finish primes.py and pass all test

--- primes.py
+++ primes.py
@@ -3,4 +3,21 @@
 
 def primes(number_of_primes):
     list = []
+    length = 0
+    index = 1
+    if number_of_primes <= 0:
+        raise ValueError("Input number is invalid")
+    while length < number_of_primes:
+        count = 0
+        for j in range(1,index + 1):
+            if index%j == 0:
+                count +=1
+        if count == 2:
+            list.append(index)
+            length +=1
+        index +=1
+            
+    
     return list
+
+print(primes(2))
